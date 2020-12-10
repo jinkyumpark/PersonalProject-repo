8d60b4edf724ff8413e7d193ffbd039aeae0e2a6
jinkyumpark <61900235+jinkyumpark@users.noreply.github.com>
2020-12-11 01:01:10 +0900

    Update MicrosoftLogoButton.java

--- Java/Chess Game/Learn Swing/Exercise/MicrosoftLogoButton.java
+++ Java/Chess Game/Learn Swing/Exercise/MicrosoftLogoButton.java
@@ -12,22 +12,23 @@ public class Frame1 extends JFrame implements ActionListener{
 	JPanel leftDown;
 	JPanel rightUp;
 	JPanel rightDown;
+	private int count = 0;
 	
 	Frame1(){
 		leftUp = new JPanel();
-		leftUp.setBackground(Color.red);
+		leftUp.setBackground(Color.gray);
 		leftUp.setBounds(0, 0, 250, 250);
 		
 		leftDown = new JPanel();
-		leftDown.setBackground(Color.blue);
+		leftDown.setBackground(Color.gray);
 		leftDown.setBounds(0, 250, 250, 500);
 		
 		rightUp = new JPanel();
-		rightUp.setBackground(Color.green);
+		rightUp.setBackground(Color.gray);
 		rightUp.setBounds(250, 0, 500, 250);
 		
 		rightDown = new JPanel();
-		rightDown.setBackground(Color.yellow);
+		rightDown.setBackground(Color.gray);
 		rightDown.setBounds(250, 250, 500, 500);
 		
 		colorButton = new JButton();
@@ -43,18 +44,27 @@ public class Frame1 extends JFrame implements ActionListener{
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
 		this.add(colorButton);
-		
+		this.add(leftUp);
+		this.add(leftDown);
+		this.add(rightUp);
+		this.add(rightDown);
 		
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if(e.getSource() == colorButton) {
-			this.add(leftUp);
-			this.add(leftDown);
-			this.add(rightUp);
-			this.add(rightDown);
+		if(e.getSource() == colorButton && count%2 == 0) {
+			leftUp.setBackground(Color.red);
+			leftDown.setBackground(Color.blue);
+			rightUp.setBackground(Color.green);
+			rightDown.setBackground(Color.yellow);
+		}else if(e.getSource() == colorButton && count%2 != 0) {
+			leftUp.setBackground(Color.gray);
+			leftDown.setBackground(Color.gray);
+			rightUp.setBackground(Color.gray);
+			rightDown.setBackground(Color.gray);
 		}
+		count++;
 	}
 }
